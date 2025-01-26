f410e3b9aae33156574243ff97e8b4030f6eae60
ajrp909 <146494885+ajrp909@users.noreply.github.com>
2025-01-26 21:19:15 +0000

    ughugh

--- src/main.py
+++ src/main.py
@@ -6,15 +6,48 @@ from dotenv import load_dotenv
 from dash import Dash, html, dash_table, dcc, callback, Output, Input
 import plotly.express as px
 
-# load_d
+# load_dotenv()
+
+# TABLE_NAME = "question"
+# DATABASE_URL = os.environ['DATABASE_URL']
+# DB_CONN = f"{DATABASE_URL}"
+
+# table_name = TABLE_NAME
+# query = "select * from {} order by question_id;"
+# conn = psycopg2.connect(DB_CONN)
+# cur = conn.cursor()
+# cur.execute(sql.SQL(query).format(sql.Identifier(table_name)))
+# columns = [column[0] for column in cur.description]
+# rows = cur.fetchall()
+# cur.close()
+# conn.close()
+
+# df = pd.DataFrame(rows, columns=columns)
+
+# scatter = df[df['difficulty'] > 0]
+
+import pandas as pd
+
+# Define data using a dictionary
+data = {
+    'Name': ['Alice', 'Bob', 'Charlie'],
+    'Age': [24, 30, 22],
+    'City': ['New York', 'Los Angeles', 'Chicago']
+}
+
+# Create DataFrame
+df = pd.DataFrame(data)
+
+# Display the DataFrame
 
 app = Dash()
 
 server = app.server
 
-app.layout = html.Div([
-    html.H1('Hello World')
-])
+app.layout = [
+    html.Div(children='My First App with Data'),
+    dash_table.DataTable(data=df.to_dict('records'), page_size=10)
+]
 
 # app.layout = [
     # html.Div(children='Personal Study Tracker Table'),
