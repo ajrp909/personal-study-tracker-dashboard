85f957e0b0bbb24d77df604d08f538d965d1514b
ajrp909 <146494885+ajrp909@users.noreply.github.com>
2025-01-26 21:11:42 +0000

    ughugh

--- src/main.py
+++ src/main.py
@@ -6,25 +6,7 @@ from dotenv import load_dotenv
 from dash import Dash, html, dash_table, dcc, callback, Output, Input
 import plotly.express as px
 
-load_dotenv()
-
-TABLE_NAME = "question"
-DATABASE_URL = os.environ['DATABASE_URL']
-DB_CONN = f"{DATABASE_URL}"
-
-table_name = TABLE_NAME
-query = "select * from {} order by question_id;"
-conn = psycopg2.connect(DB_CONN)
-cur = conn.cursor()
-cur.execute(sql.SQL(query).format(sql.Identifier(table_name)))
-columns = [column[0] for column in cur.description]
-rows = cur.fetchall()
-cur.close()
-conn.close()
-
-df = pd.DataFrame(rows, columns=columns)
-
-scatter = df[df['difficulty'] > 0]
+# load_d
 
 app = Dash()
 
