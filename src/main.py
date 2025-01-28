c4fd6c18fa59f809c97f4bcbe4d1a236076d56d8
ajrp909 <146494885+ajrp909@users.noreply.github.com>
2025-01-28 01:19:33 +0000

    interval added for dashboard data refresh

--- src/main.py
+++ src/main.py
@@ -6,52 +6,52 @@ from dotenv import load_dotenv
 from dash import Dash, html, dash_table, dcc, callback, Output, Input
 import plotly.express as px
 
-app = Dash()
-
-server = app.server
-
 load_dotenv()
 
 TABLE_NAME = "question"
 DATABASE_URL = os.environ['DATABASE_URL']
 DB_CONN = f"{DATABASE_URL}"
 
-table_name = TABLE_NAME
-query = "select * from {} order by question_id;"
-conn = psycopg2.connect(DB_CONN)
-cur = conn.cursor()
-cur.execute(sql.SQL(query).format(sql.Identifier(table_name)))
-columns = [column[0] for column in cur.description]
-rows = cur.fetchall()
-cur.close()
-conn.close()
+def fetch_data():
+    table_name = TABLE_NAME
+    query = "select * from {} order by question_id;"
+    conn = psycopg2.connect(DB_CONN)
+    cur = conn.cursor()
+    cur.execute(sql.SQL(query).format(sql.Identifier(table_name)))
+    columns = [column[0] for column in cur.description]
+    rows = cur.fetchall()
+    cur.close()
+    conn.close()
+    return pd.DataFrame(rows, columns=columns)
 
-df = pd.DataFrame(rows, columns=columns)
+app = Dash()
 
-scatter = df[df['difficulty'] > 0]
-
-app.layout = [
-    html.Div(children='My First App with Data'),
-    dash_table.DataTable(data=df.to_dict('records'), page_size=10)
-]
+server = app.server
 
 app.layout = [
     html.Div(children='Personal Study Tracker Table'),
     html.Hr(),
     dcc.Dropdown(options=['difficulty','correct', 'date'], value='difficulty', id='controls-and-radio-item'),
-    dash_table.DataTable(data=df.to_dict('records'), page_size=5),
-    dcc.Graph(figure={}, id='controls-and-graph')]
+    dash_table.DataTable(id='table', data=fetch_data().to_dict('records'), page_size=5),
+    dcc.Graph(figure={}, id='controls-and-graph'),
+    dcc.Interval(id='interval-component', interval=60*1000, n_intervals=0)]
 
 @callback(
     Output(component_id='controls-and-graph', component_property='figure'),
-    Input(component_id='controls-and-radio-item', component_property='value')
+    Output(component_id='table', component_property='data'),
+    Input(component_id='controls-and-radio-item', component_property='value'),
+    Input(component_id='interval-component', component_property='n_intervals')
 )
-def update_graph(col_chosen):
+def update(col_chosen, n):
+    df = fetch_data()
+
+    scatter = df[df['difficulty'] > 0]
+
     if col_chosen == "date":
         fig = px.bar(scatter, x=col_chosen, y='question_id')
     else:
         fig = px.scatter(scatter, x='question_id', y=col_chosen)
-    return fig
+    return fig, df.to_dict('records')
 
 if __name__ == '__main__':
     app.run_server(debug=True)
